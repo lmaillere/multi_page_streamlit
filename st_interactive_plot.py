c7fea7dcd22b87fb66867bed4ba4085c13b844e4
Ludovic Mailleret <ludovic.mailleret@inrae.fr>
2021-11-28 12:32:13 +0100

    first developments of the multipage app

--- st_interactive_plot.py
+++ st_interactive_plot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-from multipage import save, MultiPage, start_app
+from multipage import save, MultiPage, start_app, clear_cache
 
 
 
@@ -12,78 +12,128 @@ start_app() #Clears the cache when the app is started
 app = MultiPage()
 app.start_button = "Let's go!"
 app.navbar_name = "Navigation"
-app.next_page_button = "Next Page"
-app.previous_page_button = "Previous Page"
+#app.next_page_button = "Next Page"
+#app.previous_page_button = "Previous Page"
 
 def startpage():
     st.markdown("""# A test for multipage streamlit app
     using the multipage framework by Yan Almeida
     https://github.com/YanAlmeida/streamlit-multipage-framework""")
 
-app.set_initial_page(startpage)
 
-###############################################################
-# plot the logistic equation in pyplot with interaction on param
-st.title("Plot of the logistic model")
-
-st.write("This simple app takes parameters from the user and plots the corresponding logistic model, and its numerical integration.")
-
-# some x array
-x = np.arange(0, 11, 0.1)
-
-# parameter K is fixed, parameter r through user input via a streamlit slider
-K = 10.0
-r = st.slider('Enter the Intrinsic growth rate (r):',min_value=0.2, max_value=2.0, step=0.1)
-
-#############################
-# example with an integration
-# from the Biomaths code / mam3
-
-# densité initiale de la population
-x0 = st.number_input('Initial condition:', min_value = 0.0, max_value = K+2, value = 0.5, step = 0.1)
-
-# encapsulation de la densité initiale
-etat0 = np.array([x0])
-
-# encapsulation parametres
-param_logistic = np.array([r, K])
-
-# temps d'intégration
-t_0 = 0.0          
-t_fin = 20.0      
-pas_t = 0.01  
-tspan = np.arange(t_0, t_fin, pas_t)
-
+# definition du modele
 def m_logistic(etat, t, params):
     x = etat             
     r, K = params    
     xdot = r*x*(1-x/K)    
     return xdot          
 
-# intégration du modèle
-int_logistic = odeint(m_logistic, etat0, tspan, args=(param_logistic,), hmax=pas_t)
+#general variables
+# temps d'intégration
+t_0 = 0.0          
+t_fin = 20.0      
+pas_t = 0.01  
+tspan = np.arange(t_0, t_fin, pas_t)
+# some x array
+x = np.arange(0, 11, 0.1)
+
+def app1(prev_vars):
+    ###############################################################
+    # plot the logistic equation in pyplot with interaction on param
+    st.title("Plot of the logistic model")
+
+    st.write("This simple app takes parameters from the user and plots the corresponding logistic model, and its numerical integration.")
+
+    # parameter K is fixed, parameter r through user input via a streamlit slider
+    K = 10.0
+    r = st.slider('Enter the Intrinsic growth rate (r):',min_value=0.2, max_value=4.0, step=0.1)
+
+    # densité initiale de la population
+    x0 = st.number_input('Initial condition:', min_value = 0.0, max_value = K+2, value = 0.5, step = 0.1)
+
+    # encapsulation de la densité initiale
+    etat0 = np.array([x0])
+
+    # encapsulation parametres
+    param_logistic = np.array([r, K])
+
+    # intégration du modèle
+    int_logistic = odeint(m_logistic, etat0, tspan, args=(param_logistic,), hmax=pas_t)
+
+    # creation of the pyplot figure
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 5))
+    ax1.plot(x, r*x*(1-x/K), label = '$\dot x = rx(1-x/K)$')
+    ax1.grid()
+    ax1.legend()
+    ax1.set_ylim(bottom = -1, top = 6)
+    ax1.set_ylabel('dérivée $\dot x$')
+    ax1.set_xlabel('densité de population $x$')
+
+    ax2.plot(tspan, int_logistic, label = '$x(t)$', color = 'C1')
+    ax2.grid()
+    ax2.legend()
+    ax2.set_ylim(bottom = -1, top = 11)
+    ax2.set_ylabel('$x(t)$')
+    ax2.set_xlabel('temps $t$')
+
+    fig.suptitle('Modèle logistique\n $r = {}, K={}$'.format(r, K))
+
+    # show the pyplot figure in the app
+    st.pyplot(fig)
 
 
-# creation of the pyplot figure
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 5))
-ax1.plot(x, r*x*(1-x/K), label = '$\dot x = rx(1-x/K)$')
-ax1.grid()
-ax1.legend()
-ax1.set_ylim(bottom = -1, top = 6)
-ax1.set_ylabel('dérivée $\dot x$')
-ax1.set_xlabel('densité de population $x$')
+def app2(prev_vars):
+    ###############################################################
+    # plot the logistic equation in pyplot with interaction on param
+    st.title("Plot of the logistic model 2")
 
-ax2.plot(tspan, int_logistic, label = '$x(t)$', color = 'C1')
-ax2.grid()
-ax2.legend()
-ax2.set_ylim(bottom = -1, top = 11)
-ax2.set_ylabel('$x(t)$')
-ax2.set_xlabel('temps $t$')
-
-fig.suptitle('Modèle logistique\n $r = {}, K={}$'.format(r, K))
-
-# show the pyplot figure in the app
-st.pyplot(fig)
+    st.write("This simple app takes parameters from the user and plots the corresponding logistic model, and its numerical integration.")
 
 
+    # parameter K is fixed, parameter r through user input via a streamlit slider
+    K = 10.0
+    r = st.slider('Enter the Intrinsic growth rate (r):',min_value=0.2, max_value=2.0, step=0.1)
+
+    #############################
+    # example with an integration
+    # from the Biomaths code / mam3
+
+    # densité initiale de la population
+    x0 = st.number_input('Initial condition:', min_value = 0.0, max_value = K+2, value = 0.5, step = 0.1)
+
+    # encapsulation de la densité initiale
+    etat0 = np.array([x0])
+
+    # encapsulation parametres
+    param_logistic = np.array([r, K])
+
+    # intégration du modèle
+    int_logistic = odeint(m_logistic, etat0, tspan, args=(param_logistic,), hmax=pas_t)
+
+    # creation of the pyplot figure
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 5))
+    ax1.plot(x, r*x*(1-x/K), label = '$\dot x = rx(1-x/K)$')
+    ax1.grid()
+    ax1.legend()
+    ax1.set_ylim(bottom = -1, top = 6)
+    ax1.set_ylabel('dérivée $\dot x$')
+    ax1.set_xlabel('densité de population $x$')
+
+    ax2.plot(tspan, int_logistic, label = '$x(t)$', color = 'C1')
+    ax2.grid()
+    ax2.legend()
+    ax2.set_ylim(bottom = -1, top = 11)
+    ax2.set_ylabel('$x(t)$')
+    ax2.set_xlabel('temps $t$')
+
+    fig.suptitle('Modèle logistique\n $r = {}, K={}$'.format(r, K))
+
+    # show the pyplot figure in the app
+    st.pyplot(fig)
+
+
+# setup of the whole multipage app
+app.set_initial_page(startpage)
+app.add_app("logistic model", app1)
+app.add_app("logistic model 2", app2)
 app.run()
