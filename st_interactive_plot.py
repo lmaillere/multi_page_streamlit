c37b18fdd16353d61d1d2af2c4e0acb3caba894d
Ludovic Mailleret <ludovic.mailleret@inrae.fr>
2021-11-28 11:32:07 +0100

    update py script

--- st_interactive_plot.py
+++ st_interactive_plot.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+from multipage import MultiPage
 
 ###############################################################
 # plot the logistic equation in pyplot with interaction on param
